6891249f4f2391e6cc0b7e09e5e1c8c46d246e52
Pranai Rao <pranai@rao.me>
2024-04-01 13:31:00 -0400

    Cleaned up import * statements to only include relevant information.

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -1,10 +1,23 @@
 package game;
-import java.awt.*;
-import java.awt.event.*;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.*;
-import java.awt.Color;
+
+import javax.swing.JPanel;
 
 import pieces.ChessPiece;
 import pieces.ChessPosition;
--- src/pieces/ChessPiece.java
+++ src/pieces/ChessPiece.java
@@ -2,15 +2,22 @@ package pieces;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 import javax.swing.ImageIcon;
 
 import game.GameBoard;
 
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Color;
 
 public class ChessPiece {
     static int BASE = 0b1000000000000000000000000000000;
