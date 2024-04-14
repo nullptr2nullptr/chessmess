15850a9dd6a4f90b3e635c53005be973c6971638
Pranai Rao <pranai@rao.me>
2024-04-14 17:28:59 -0400

    Fixed sound path

--- src/pieces/Sounds.java
+++ src/pieces/Sounds.java
@@ -1,7 +1,7 @@
 package pieces;
 
 public class Sounds {
-    public final static String MOVE_SOUND_FILE = "src/res/sound/loudMove.wav";
+    public final static String MOVE_SOUND_FILE = "src/res/sound/move.wav";
     public final static String PEW_SOUND_FILE = "src/res/sound/pew.wav";
     public final static String TROMBONE_SOUND_FILE = "src/res/sound/trombone.wav";
     public final static String LEGO_SOUND_FILE = "src/res/sound/lego.wav";
